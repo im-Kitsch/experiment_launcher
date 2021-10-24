3f42d339980f9d6340d5cc5c77e92b6fe7caf942
Zhiyuan <zhiyuan.hu@stud.tu-darmstadt.de>
2021-10-25 00:13:56 +0200

    fixed

--- experiment_launcher/launcher.py
+++ experiment_launcher/launcher.py
@@ -195,7 +195,7 @@ echo "SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_ID JOBLIB_SEEDS $JOBLIB_SEEDS"
 
 #  module list
 
-makedir -p {self._exp_dir_slurm}
+mkdir -p $1
 ln -s  {self._exp_dir_slurm}/"$SLURM_JOB_ID"_"$SLURM_ARRAY_TASK_ID".out  $1/"$SLURM_JOB_ID"_"$SLURM_ARRAY_TASK_ID".out 
 ln -s  {self._exp_dir_slurm}/"$SLURM_JOB_ID"_"$SLURM_ARRAY_TASK_ID".err  $1/"$SLURM_JOB_ID"_"$SLURM_ARRAY_TASK_ID".err   
 # not sure totally right
